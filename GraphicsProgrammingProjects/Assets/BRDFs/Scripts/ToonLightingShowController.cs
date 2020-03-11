1b00a287a40dea6ddf4893a16e02de67156fb157
derek-glennon <derek.r.glennon@gmail.com>
2020-03-10 23:35:06 -0500

    Adding Toon Lighting Show
    Showing off toon ramp lighting

---
+++ GraphicsProgrammingProjects/Assets/BRDFs/Scripts/ToonLightingShowController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToonLightingShowController : MonoBehaviour
+{
+    [SerializeField]
+    private float showDuration;
+    [SerializeField]
+    private Transform arrow;
+    [SerializeField]
+    private Transform pointerBW;
+    [SerializeField]
+    private Transform pointerStylized;
+    [SerializeField]
+    private Transform pointerRamp;
+    [SerializeField]
+    private Transform endPointBW;
+    [SerializeField]
+    private Transform endPointStlyized;
+    [SerializeField]
+    private Transform endPointRamp;
+
+    private Vector3 startingPositionBW;
+    private Vector3 startingPositionStylized;
+    private Vector3 startingPositionRamp;
+
+    private float t = 0.0f;
+
+    float Remap(float x, float a, float b, float c, float d)
+    {
+        return (c + (x - a) * ((d - c) / (b - a)));
+    }
+
+    void Awake()
+    {
+        t = 0.0f;
+
+        startingPositionBW = pointerBW.position;
+        startingPositionStylized = pointerStylized.position;
+        startingPositionRamp = pointerRamp.position;
+    }
+
+    void Update()
+    {
+        t = t > showDuration ? 0.0f : t + Time.deltaTime;
+
+        float tRemap = Remap(t, 0.0f, showDuration, 0.0f, 1.0f);
+
+        //Rotate Arrow
+        Vector3 newRotation = Vector3.Lerp(new Vector3(0.0f, 180f, 0.0f), new Vector3(0.0f, (180f - 360f), 0.0f), tRemap);
+        Vector3 spinAmount = newRotation - arrow.rotation.eulerAngles;
+        arrow.rotation = Quaternion.Euler(arrow.rotation.eulerAngles + spinAmount);
+
+        //Move Pointers
+        pointerBW.position = Vector3.Lerp(startingPositionBW, endPointBW.position, tRemap);
+        pointerStylized.position = Vector3.Lerp(startingPositionStylized, endPointStlyized.position, tRemap);
+        pointerRamp.position = Vector3.Lerp(startingPositionRamp, endPointRamp.position, tRemap);
+
+    }
+}
---
+++ GraphicsProgrammingProjects/Assets/BRDFs/Shaders/BlinnPhongBRDF_Toon.shader
@@ -0,0 +1,77 @@
+﻿Shader "BRDF/BlinnPhongBRDF_Toon"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (1,1,1,1)
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_RampTex("Toon Ramp Tex", 2D) = "white" {}
+		_SpecularPower ("Specular Power", Range(0,128)) = 128
+		_SpecularColor ("Specular Color", Color) = (1,1,1,1)
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+
+        #pragma surface surf BlinnPhongBRDF fullforwardshadows
+		#pragma target 3.0
+
+		struct SurfaceOutputBRDF
+		{
+			fixed3 Albedo;
+			fixed Alpha;
+			fixed3 Normal;
+			fixed3 Emission;
+		};
+
+		sampler2D _RampTex;
+		float _SpecularPower;
+		fixed4 _SpecularColor;
+
+		half4 LightingBlinnPhongBRDF(SurfaceOutput s, half3 lightDir, half3 viewDir, half atten)
+		{
+			float3 normalDir = normalize(s.Normal);
+
+			half NdotL = dot(normalDir, lightDir);
+
+			half remappedNdotL = NdotL * 0.5 + 0.5;
+			half2 toonUV = half2(remappedNdotL, 0.5);
+			fixed3 toonSample = tex2D(_RampTex, toonUV);
+
+			half3 diffuse = s.Albedo * toonSample;
+
+			//half3 H = normalize(lightDir + viewDir);
+			//float NdotH = max(0, dot(normalDir, H));
+			//float specularIntensity = pow(saturate(NdotH), _SpecularPower);
+			//float3 spec = specularIntensity * _SpecularColor.rgb;
+
+			half4 c;
+			//c.rgb = (diffuse + spec) * _LightColor0.rgb;
+			c.rgb = (diffuse) * _LightColor0.rgb;
+			c.a = s.Alpha;
+			return c;
+		}
+
+        sampler2D _MainTex;
+
+        struct Input
+        {
+            float2 uv_MainTex;
+        };
+
+        half _Glossiness;
+        half _Metallic;
+        fixed4 _Color;
+
+        void surf (Input IN, inout SurfaceOutput o)
+        {
+            fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+            o.Albedo = c.rgb;
+            o.Alpha = c.a;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
